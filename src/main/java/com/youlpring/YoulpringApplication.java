4fa044761340c00e28c0ecee8781e2fbe004c3a1
jiyoul <beatmejy@naver.com>
2023-11-20 20:16:52 +0900

    [feat] 최초 프로젝트 생성!

---
+++ src/main/java/com/youlpring/YoulpringApplication.java
@@ -0,0 +1,13 @@
+package com.youlpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoulpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoulpringApplication.class, args);
+	}
+
+}
