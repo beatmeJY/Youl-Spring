8743c68cdf8ac9e1e1887b3634b8798a47c4becb
beatmejy <beatmejy@naver.com>
2023-12-13 04:54:06 +0900

    [chore] 메인 클래스 명 변경.

--- src/main/java/com/youlpring/YoulpringApplication.java
+++ src/main/java/com/youlpring/YoulpringApplication.java
@@ -3,7 +3,7 @@ package com.youlpring;
 
 import com.youlpring.tomcat.apache.catalina.startup.Tomcat;
 
-public class Application {
+public class YoulpringApplication {
 
     public static void main(String[] args) {
         final var tomcat = new Tomcat();
