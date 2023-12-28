df78eafd6dbbb4d9caa097f0d198971b8b1b8291
beatmejy <beatmejy@naver.com>
2023-12-29 01:23:18 +0900

    [feat] 추후 동적 웹페이지 보여주기 위해 ModelAndView 추가.

--- src/main/java/com/youlpring/tomcat/apache/coyote/http11/response/HttpResponse.java
+++ src/main/java/com/youlpring/tomcat/apache/coyote/http11/response/HttpResponse.java
@@ -1,6 +1,8 @@
 package com.youlpring.tomcat.apache.coyote.http11.response;
 
+import com.youlpring.jws.controller.ModelAndView;
 import com.youlpring.tomcat.apache.coyote.http11.constants.HttpHeaderConstant;
+import com.youlpring.tomcat.apache.coyote.http11.enums.ContentType;
 import com.youlpring.tomcat.apache.coyote.http11.enums.HttpProtocol;
 import com.youlpring.tomcat.apache.coyote.http11.enums.HttpStatus;
 import org.slf4j.Logger;
@@ -14,38 +16,68 @@ public class HttpResponse {
 
     private HttpProtocol httpProtocol = HttpProtocol.HTTP_1_1;
     private HttpStatus httpStatus = HttpStatus.OK;
-    private ResponseHeader responseHeader;
+    private final ResponseHeader responseHeader = new ResponseHeader();
     private ResponseBody responseBody;
+    private final ModelAndView modelAndView = new ModelAndView();
 
-    public HttpResponse(ResponseBody responseBody) throws IOException {
-        this.responseHeader = new ResponseHeader();
-        if (responseBody == null || responseBody.getBodyLength() == 0) {
+    public HttpResponse() throws IOException {}
+
+    public void setResponseBody(ResponseBody body) {
+        if (body == null || body.getBodyLength() == 0) {
             return;
         }
-        this.responseBody = responseBody;
-        responseHeader.setHeader(HttpHeaderConstant.CONTENT_LENGTH, responseBody.getBodyLength());
-        if (responseBody.getContentType() != null) {
-            responseHeader.setHeader(HttpHeaderConstant.CONTENT_TYPE, responseBody.getContentType());
+        this.responseBody = body;
+        responseHeader.setHeader(HttpHeaderConstant.CONTENT_LENGTH, this.responseBody.getBodyLength());
+        if (this.responseBody.getContentType() != null) {
+            responseHeader.setHeader(HttpHeaderConstant.CONTENT_TYPE, this.responseBody.getContentType());
         }
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        if (httpStatus != null) {
+            this.httpStatus = httpStatus;
+        }
+    }
+
+    public void setServerRedirect(String url) {
+        setHttpStatus(HttpStatus.FOUND);
+        responseHeader.setHeader("Location", url);
+    }
+
+    public void setClientRedirect(String url, boolean successFlag) {
+        if (successFlag) {
+            setHttpStatus(HttpStatus.OK);
+        } else {
+            setHttpStatus(HttpStatus.FOUND);
+        }
+        setResponseBody(new ResponseBody(url, ContentType.TEXT_PLAIN));
+    }
+
+    public void setValue(String key, Object value) {
+        modelAndView.setModel(key, value);
+    }
+
+    public void setViewName(String viewName) {
+        modelAndView.setViewName(viewName);
     }
 
     private String getFirstHeader() {
         return httpProtocol.getProtocol() + " " + httpStatus.getHeaderString();
     }
 
-    public ResponseHeader getResponseHeader() {
-        return responseHeader;
+
+    public ModelAndView getModelAndView() {
+        return modelAndView;
     }
 
-    public ResponseBody getResponseBody() {
-        return responseBody;
+    public String getViewName() {
+        return modelAndView.getViewName();
     }
 
     public byte[] getHttpByte() {
         StringBuilder builder = new StringBuilder();
         builder.append(this.getFirstHeader()).append("\r\n");
         builder.append(this.responseHeader.getHeaderString()).append("\r\n");
-        builder.append("\r\n");
         if (responseBody != null && responseBody.getBodyLength() != 0) {
             builder.append(responseBody.getBody());
         }
