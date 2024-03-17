7cfbce7710c6a48ab939b32b4898d877ff9dc61c
beatmejy <beatmejy@naver.com>
2024-03-17 22:52:42 +0900

    [test] File 및 IO Util 클래스 테스트 추가.

---
+++ src/test/java/com/youlpring/Fixture/utill/FileFixture.java
@@ -0,0 +1,35 @@
+package com.youlpring.Fixture.utill;
+
+import com.youlpring.tomcat.apache.util.FileUtil;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileFixture {
+
+    public static final String STATIC_FILE = "/youlpring.txt";
+    public static final String DYNAMIC_URL = "/index";
+
+    public static final String STATIC_FILE_PATH = "static/youlpring.txt";
+    public static final String DYNAMIC_FILE_PATH = "templates/index.html";
+
+
+    public static String readStaticTextFile() {
+        try {
+            URI uri = FileUtil.class.getClassLoader().getResource(FileFixture.STATIC_FILE_PATH).toURI();
+            return Files.readString(Path.of(uri));
+        } catch (Exception e) {
+            throw new RuntimeException("정적 파일 읽기에 실패하였습니다.");
+        }
+    }
+
+    public static String readDynamicFile() {
+        try {
+            URI uri = FileUtil.class.getClassLoader().getResource(FileFixture.DYNAMIC_FILE_PATH).toURI();
+            return Files.readString(Path.of(uri));
+        } catch (Exception e) {
+            throw new RuntimeException("동적 파일 읽기에 실패하였습니다.");
+        }
+    }
+}
