b6326d82a74dae5f8bf6e6a563e5c0b1c435f6f5
beatmejy <beatmejy@naver.com>
2024-04-08 04:42:26 +0900

    [test] 동적페이지 처리 테스트 수정 및 추가.

--- src/test/java/com/youlpring/jws/controller/FrontControllerTest.java
+++ src/test/java/com/youlpring/jws/controller/FrontControllerTest.java
@@ -1,6 +1,5 @@
 package com.youlpring.jws.controller;
 
-import com.youlpring.Fixture.tomcat.coyote.http11.ResponseFixture;
 import com.youlpring.tomcat.apache.coyote.http11.enums.HttpMethod;
 import com.youlpring.tomcat.apache.coyote.http11.request.HttpRequest;
 import com.youlpring.tomcat.apache.coyote.http11.response.HttpResponse;
@@ -9,7 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static com.youlpring.Fixture.tomcat.coyote.http11.RequestFixture.HOME_REQUEST_URL;
+import static com.youlpring.Fixture.tomcat.coyote.http11.ResponseFixture.getExpectedHomePageNotLoggedIn;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @DisplayName("[Unit] FrontController 테스트")
 class FrontControllerTest {
@@ -17,13 +19,13 @@ class FrontControllerTest {
     @Test
     @DisplayName("컨트롤러를 통해 동적 요청을 처리한다.")
     void processSuccess() {
-        HttpRequest mockRequest = Mockito.mock(HttpRequest.class);
+        HttpRequest mockRequest = mock(HttpRequest.class);
         HttpResponse response = new HttpResponse();
-        Mockito.when(mockRequest.getUrl()).thenReturn(HOME_REQUEST_URL);
-        Mockito.when(mockRequest.getMethod()).thenReturn(HttpMethod.GET);
+        when(mockRequest.getUrl()).thenReturn(HOME_REQUEST_URL);
+        when(mockRequest.getMethod()).thenReturn(HttpMethod.GET);
 
         FrontController.process(mockRequest, response);
 
-        assertArrayEquals(ResponseFixture.getExpectedHomePage().getBytes(), response.getHttpByte());
+        assertArrayEquals(getExpectedHomePageNotLoggedIn().getBytes(), response.getHttpByte());
     }
 }
--- src/test/java/com/youlpring/jws/controller/ViewResolverTest.java
+++ src/test/java/com/youlpring/jws/controller/ViewResolverTest.java
@@ -5,31 +5,34 @@ import com.youlpring.tomcat.apache.coyote.http11.request.HttpRequest;
 import com.youlpring.tomcat.apache.coyote.http11.response.HttpResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import static com.youlpring.Fixture.tomcat.coyote.http11.ResponseFixture.getExpectedHomePage;
-import static com.youlpring.Fixture.tomcat.coyote.http11.ResponseFixture.getHttpResponseOnlyFirstHeader;
+import static com.youlpring.Fixture.tomcat.coyote.http11.ResponseFixture.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @DisplayName("[Unit] ViewResolver 테스트")
 class ViewResolverTest {
 
     @Test
-    @DisplayName("타임리프를 통해 홈페이지 반환에 성공한다.")
+    @DisplayName("타임리프를 통해 동적 홈페이지 반환에 성공한다.")
     void executeSuccess() {
-        HttpRequest request = Mockito.mock(HttpRequest.class);
+        HttpRequest request = mock(HttpRequest.class);
         HttpResponse response = new HttpResponse();
         response.setViewName(RequestFixture.HOME_REQUEST_URL);
 
         ViewResolver.execute(request, response);
 
-        assertArrayEquals(getExpectedHomePage().getBytes(), response.getHttpByte());
+        assertArrayEquals(getExpectedHomePageNotLoggedIn().getBytes(), response.getHttpByte());
+
+        when(request.isLogin()).thenReturn(true);
+        assertArrayEquals(getExpectedHomePageLoggedIn().getBytes(), response.getHttpByte());
     }
 
     @Test
     @DisplayName("ViewName 이 없으면 뷰는 반환하지 않는다.")
     void executeFail() {
-        HttpRequest request = Mockito.mock(HttpRequest.class);
+        HttpRequest request = mock(HttpRequest.class);
         HttpResponse response = new HttpResponse();
 
         ViewResolver.execute(request, response);
